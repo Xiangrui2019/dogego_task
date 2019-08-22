b54f06c3494abcb87d3d234d4d40116eb03c57f0
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 22:15:04 +0800

    sdafd

--- conf/config.go
+++ conf/config.go
@@ -1,1 +1,40 @@
 package conf
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type Item struct {
+	Name        string `json:"name"`
+	CallbackURL string `json:"callback_url"`
+	Time        string `json:"time"`
+}
+
+type Config struct {
+	Tasks []Item `json:"tasks"`
+}
+
+var config *Config
+
+func init() {
+	fp, err := os.Open("./config.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(fp)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(data, &config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
