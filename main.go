98387957b739127815b4a2ef04cbd262baa8f4f9
Go Go Go <xiangrui@aiursoft.com>
2019-08-23 07:29:32 +0800

    dsa

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	_ "dogego_task/conf"
 	"dogego_task/servers"
+	"log"
 )
 
 func main() {
 	server := servers.NewJobServer()
 	server.RegisterCronTask()
+
+	log.Println("Started cron tasks..")
+	server.CronServer.Start()
 }
